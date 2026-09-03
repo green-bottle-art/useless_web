1a198725a2ee7a57d5c3677b505dbfdfab6967a2
ari <ari.me>
2026-09-03 12:37:19 +0700

    pindah css

--- cardGame/src/main.jsx
+++ cardGame/src/main.jsx
@@ -6,6 +6,6 @@ function NavigationBar() {
   return <h1>Hello from React!</h1>;
 }
 
-const domNode = document.getElementById('pixi-container');
+const domNode = document.getElementById('app');
 const root = createRoot(domNode);
 root.render(<NavigationBar />);

f884fd14c1963665655c0c2e0c36be0039451bb7
ari <ari.me>
2026-09-03 12:47:16 +0700

    inisialisasi pixi

--- cardGame/src/main.jsx
+++ cardGame/src/main.jsx
@@ -1,11 +1,29 @@
-import { Application, Assets, Container, Sprite } from "pixijs";
+import { Application, Text, Graphics} from "pixijs";
 import { createRoot } from 'react-dom/client';
+import React, { useState, useEffect, useRef } from 'react';
 
-function NavigationBar() {
-  // TODO: Actually implement a navigation bar
-  return <h1>Hello from React!</h1>;
+function PixiCanvas({scene, onGameOver}) {
+  const containerRef = useRef(null);
+  const appRef = useRef(null);
+
+
+  // Inisialisasi
+  useEffect(() => {
+    let isMounted = true;
+    const app = new Application();
+
+    async function initPixi() {
+      await app.init({
+        width: 800,
+        height: 600,
+        backgroundColor: 0x1e1e2e,
+      });
+
+      if (!isMounted) return;
+    }
+  })
 }
 
 const domNode = document.getElementById('app');
 const root = createRoot(domNode);
-root.render(<NavigationBar />);
+root.render(<App />);
